7eb2f24749138235b301ba7cabe320bdd8736f96
Lara Costa <lara.estefanypc@gmail.com>
2022-10-28 16:52:13 -0300

    Base para criação dos códigos da página.

---
+++ codigo.js
@@ -0,0 +1,29 @@
+// RESULTADOS: classe: animalPerson
+// VOCÊ É UM CATPERSON!
+// VOCÊ É UM DOGPERSON!
+// document.createElement("h2")
+// h2 innerText = resultado
+
+// API FATOS GATOS
+// var fatosGatos = document.getelementbyId("fatosGatos")
+// document.createElement("h3")
+// máximo 95 caracteres
+
+// API FATOS CACHORROS
+// var fatosCachorros = document.getelementbyId("fatosCachorros")
+// document.createElement("h3")
+// máximo 95 caracteres
+
+// API FOTOS GATOS
+// var fotosGatos = document.getelementbyId("fotosGatos")
+// document.createElement("img")
+
+// API FOTOS CACHORROS
+//  var fotosCachorros = document.getelementbyId("fotosCachorros")
+// document.createElement("img")
+
+// PLACAR
+// var placarGato = document.getelementbyId("pontoGato")
+// document.createElement("p")
+// var placarCachorro = document.getelementbyId("pontoCachorro")
+// document.createElement("p")
