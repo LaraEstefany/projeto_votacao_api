c9d7663205654ac735858a7bcf3669dbcea4f76b
Lara Costa <lara.estefanypc@gmail.com>
2022-10-28 19:18:05 -0300

    Adição das URL das API's.

--- codigo.js
+++ codigo.js
@@ -8,22 +8,28 @@
 // var fatosGatos = document.getelementbyId("fatosGatos")
 // document.createElement("h3")
 // máximo 95 caracteres
+// https://catfact.ninja/fact?max_length=95
 
 // API FATOS CACHORROS
 // var fatosCachorros = document.getelementbyId("fatosCachorros")
 // document.createElement("h3")
 // máximo 95 caracteres
+// https://dog-api.kinduff.com/api/facts (NÃO TEM COMO ESPECIFICAR TAMAMHO)
 
 // API FOTOS GATOS
 // var fotosGatos = document.getelementbyId("fotosGatos")
 // document.createElement("img")
+// https://api.thecatapi.com/v1/images/search
 
 // API FOTOS CACHORROS
 //  var fotosCachorros = document.getelementbyId("fotosCachorros")
 // document.createElement("img")
+// https://dog.ceo/api/breeds/image/random
 
 // PLACAR
 // var placarGato = document.getelementbyId("pontoGato")
 // document.createElement("p")
 // var placarCachorro = document.getelementbyId("pontoCachorro")
 // document.createElement("p")
+
+
